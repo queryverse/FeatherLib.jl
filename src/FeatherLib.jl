47bae09e92945a222b4209b8a704232cfa638e72
David Anthoff <anthoff@berkeley.edu>
2018-03-25 12:24:09 -0700

    Track upstream work
    This ports commit b5bc7c53204e63cc0ab3c7a94e7746d8cec7be23 by
    ExpandingMan over.

--- src/FeatherLib.jl
+++ src/FeatherLib.jl
@@ -27,6 +27,7 @@ const FEATHER_VERSION = 2
 # wesm/feather/cpp/src/common.h
 const FEATHER_MAGIC_BYTES = Vector{UInt8}(codeunits("FEA1"))
 const ALIGNMENT = 8
+const MIN_FILE_LENGTH = 12
 
 
 include("metadata.jl")  # flatbuffer defintions
